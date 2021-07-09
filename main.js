9186a65f94d2aca20c51644fbc03527418f4032f
BasharAlZawahreh <zawahrehbashar@gmail.com>
2021-07-09 19:40:33 +0300

    LAST

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ console.log(arrowTitle)
 
 console.log('*********Pulse Check**********');
 console.log('Q1');
-let myFavoriteFood  = 'Maqlouba';
+let myFavoriteFood = 'Maqlouba';
 // make sure that the variable is in the global scope
 const favoriteFood = function () {
     return myFavoriteFood;
 };
 
-favoriteFood(); // => the value of `myFavoriteFood` variable
+//favoriteFood(); // => the value of `myFavoriteFood` variable
 
 
 console.log('Q2');
@@ -18,8 +18,8 @@ const updateFavoriteFood = function (newValue) {
     myFavoriteFood = newValue;
 };
 
-updateFavoriteFood("Pizza");
-favoriteFood(); // => "Pizza"
+//updateFavoriteFood("Pizza");
+//favoriteFood(); // => "Pizza"
 
 
 
@@ -27,7 +27,7 @@ console.log('Q2');
 console.log('Q3');
 const createCounter = function (start) {
     let counter = start;
-    
+
     // this anonymous function is a closure function that has access to its own closure variable
     return function () {
         return ++counter;
@@ -46,7 +46,7 @@ console.log('Q1');
 
 let age = 25;
 if (true) {
-  age = 30;
+    age = 30;
 }
 age; // Always the value of age will be changed to 30 because the condition is always true
 
@@ -56,7 +56,7 @@ if (true) {
     const myName = "Jane";
 }
 
- myName; // here we will face an Error of changing a constant value
+myName; // here we will face an Error of changing a constant value
 
 
 
@@ -64,25 +64,25 @@ if (true) {
 console.log('Q2');
 let number = 10;
 const func1 = function () {
-  let number = 15;
+    let number = 15;
 
-  if (true) {
-    let number = 24;
-  }
+    if (true) {
+        let number = 24;
+    }
 
-  return number;
+    return number;
 };
 
 func1(); // Error of redeclaring the variable number!
 
 const func2 = function (age) {
-  age = 10;
-  if (true) {
-    let age = 24;
-    age = 20;
-  }
+    age = 10;
+    if (true) {
+        let age = 24;
+        age = 20;
+    }
 
-  return age;
+    return age;
 };
 
 func2(26); // Error of redeclaring the variable age!
@@ -98,14 +98,14 @@ const countDown = function () {
 
     return --count;
 };
-  
-  //countDown(); // => 4
-  //countDown(); // => 3
-  //countDown(); // => 2
-  //countDown(); // => 1
-  //countDown(); // => 0
-  //countDown(); // => "count down is over"
-  
+
+//countDown(); // => 4
+//countDown(); // => 3
+//countDown(); // => 2
+//countDown(); // => 1
+//countDown(); // => 0
+//countDown(); // => "count down is over"
+
 
 
 console.log('Q4');
@@ -114,13 +114,13 @@ console.log('Q4');
 const countUp = function () {
     return ++count;
 };
-  
- //countUp(); // => 4
- //countUp(); // => 5
- //countUp(); // => 6
- //countUp(); // => 7
- //countUp(); // => 8
-  
+
+//countUp(); // => 4
+//countUp(); // => 5
+//countUp(); // => 6
+//countUp(); // => 7
+//countUp(); // => 8
+
 
 
 
@@ -129,28 +129,28 @@ console.log('Q5');
 const resetCount = function (start) {
     count = start;
 };
-  
-  //resetCount(0); // => "the count has been reset"
-  //countUp(); // => 1
-  //resetCount(10); // => "the count has been reset"
-  //countUp(); // => 11
-  
+
+//resetCount(0); // => "the count has been reset"
+//countUp(); // => 1
+//resetCount(10); // => "the count has been reset"
+//countUp(); // => 11
+
 
 
 
 
 console.log('Q6');
-console.log('Q7');//********************************************************************************************** */
+console.log('Q7');
 const createToDoList = function (toDo) {
     let toDoList = '';
-    
-    return function(){
+
+    return function (toDo) {
         toDoList += toDo + ' ';
-        
+
         return toDoList;
     }
 
-    
+
 };
 
 
@@ -162,13 +162,13 @@ const toDoListOne = createToDoList();
 console.log('Q8');
 let accountBalance = 0;
 const deposit = function (amount) {
-    accountBalance += amount; 
-    return   accountBalance;
+    accountBalance += amount;
+    return accountBalance;
 };
-  
-  //deposit(100); // => 100
-  //deposit(50); // => 150
-  
+
+//deposit(100); // => 100
+//deposit(50); // => 150
+
 
 
 
@@ -179,20 +179,53 @@ const withdraw = function (amount) {
         return 'insufficient funds, current balance: 30';
     }
 
-   accountBalance -= amount;
-   return accountBalance;
+    accountBalance -= amount;
+    return accountBalance;
 };
-  
-  //deposit(100); // => 100
-  //deposit(50); // => 150
-  //withdraw(70); // => 80
-  //deposit(50); // => 130
-  //withdraw(100); // => 30
-  //withdraw(100); // => "insufficient funds, current balance: 30"
 
-  
+//deposit(100); // => 100
+//deposit(50); // => 150
+//withdraw(70); // => 80
+//deposit(50); // => 130
+//withdraw(100); // => 30
+//withdraw(100); // => "insufficient funds, current balance: 30"
 
-console.log('Q10');/**************************************************************************** */
+
+
+console.log('Q10');
+const createAccount = function (initialValue) {
+
+    let balance = initialValue;
+
+    return  function(transactionType , amount){
+        if (transactionType === 'withdraw') {
+            if (balance < amount) {
+                return `Insufficient funds, current balance: ${balance}`
+            }
+            else{
+                balance -= amount;
+            }
+        }
+        else if(transactionType ==='deposit'){
+            balance += amount;
+        }
+
+        return balance;
+    };
+
+};
+
+const accountOne = createAccount(0);
+//accountOne("withdraw", 10); // => "insufficient funds, current balance: 0"
+//accountOne("deposit", 50); // => 50
+//accountOne("deposit", 70); // => 120
+//accountOne("withdraw", 10); // => 110
+
+const accountTwo = createAccount(500);
+//accountTwo("withdraw", 100); // => 400
+//accountTwo("withdraw", 100); // => 300
+//accountTwo("deposit", 50); // => 350
+//accountTwo("withdraw", 500); // => "insufficient funds, current balance: 350"
 
 
 
@@ -219,14 +252,14 @@ const minMax = function (number) {
 
     return `The maximum number is: ${max} and the minimum number is ${min}`
 
-  };
-  
-  //minMax(5); // => "the maximum number is: 5 and the minimum number is 5"
-  //minMax(2); // => "the maximum number is: 5 and the minimum number is 2"
-  //minMax(3); // => "the maximum number is: 5 and the minimum number is 2"
-  //minMax(7); // => "the maximum number is: 7 and the minimum number is 2"
-  //minMax(0); // => "the maximum number is: 7 and the minimum number is 0"
-  
+};
+
+//minMax(5); // => "the maximum number is: 5 and the minimum number is 5"
+//minMax(2); // => "the maximum number is: 5 and the minimum number is 2"
+//minMax(3); // => "the maximum number is: 5 and the minimum number is 2"
+//minMax(7); // => "the maximum number is: 7 and the minimum number is 2"
+//minMax(0); // => "the maximum number is: 7 and the minimum number is 0"
+
 
 
 
@@ -236,51 +269,51 @@ console.log('Q3');
 console.log('Q5');
 let wins = 0;
 let losts = 0;
-let computerMove  = '';
+let computerMove = '';
 
 const randomMove = function () {
-    const move =   Math.random();
-  
-    if (computerMove  !== '') {
-        if (move <= (1/3)) {
-            return  'rock';
+    const move = Math.random();
+
+    if (computerMove !== '') {
+        if (move <= (1 / 3)) {
+            return 'rock';
         }
-        else if (move <= (2/3)) {
-            return  'paper';
+        else if (move <= (2 / 3)) {
+            return 'paper';
         }
         else {
-            return  'scissors';
+            return 'scissors';
         }
-        
+
     }
-    
-    else{
-        if (move <= (1/4)) {
-            return  'rock';
+
+    else {
+        if (move <= (1 / 4)) {
+            return 'rock';
         }
-        else if (move <= (2/4)) {
-            return  'paper';
+        else if (move <= (2 / 4)) {
+            return 'paper';
         }
-        else if(move <= (2/4)) {
-            return  'scissors';
+        else if (move <= (2 / 4)) {
+            return 'scissors';
         }
-        else{
+        else {
             return computerMove;
         }
     }
 
-  
+
 };
 
 
-const rockPaperScissors = function (move,reset) {
-    
-     computerMove = randomMove();
+const rockPaperScissors = function (move, reset) {
+
+    computerMove = randomMove();
     //lastMove = randomMove();
 
-   if (reset && move === '') {
-       return "The game has been resetted";
-   } 
+    if (reset && move === '') {
+        return "The game has been resetted";
+    }
 
     if (wins === 3 || losts === 3 || reset) {
         wins = losts = 0;
@@ -288,26 +321,26 @@ const rockPaperScissors = function (move,reset) {
 
 
 
-  if (move !== 'rock' && move !== 'scissors' && move !== 'paper') {
-      return  "invalid move";
-  }     
+    if (move !== 'rock' && move !== 'scissors' && move !== 'paper') {
+        return "invalid move";
+    }
 
-  if (computerMove === move) {
-      return 'its a draw';
-  }
-  else if ((computerMove === 'rock' &&  move === 'paper') ||
-           (computerMove === 'scissors' &&  move === 'rock') ||
-           (computerMove === 'paper' &&  move === 'scissors')
-  ) {
-     
-     return `Won: ${++wins}, Lost:${losts}`;
-  }
-  else if ((move === 'rock' &&  computerMove === 'paper') ||
-           (move === 'scissors' &&  computerMove === 'rock') ||
-           (move === 'paper' &&  computerMove === 'scissors')
-  ) {
-     return  `Won: ${wins}, Lost:${++losts}`;
-  }
+    if (computerMove === move) {
+        return 'its a draw';
+    }
+    else if ((computerMove === 'rock' && move === 'paper') ||
+        (computerMove === 'scissors' && move === 'rock') ||
+        (computerMove === 'paper' && move === 'scissors')
+    ) {
+
+        return `Won: ${++wins}, Lost:${losts}`;
+    }
+    else if ((move === 'rock' && computerMove === 'paper') ||
+        (move === 'scissors' && computerMove === 'rock') ||
+        (move === 'paper' && computerMove === 'scissors')
+    ) {
+        return `Won: ${wins}, Lost:${++losts}`;
+    }
 
 };
 
