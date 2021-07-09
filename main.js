3c2d681ca8cc6ea0108c10d9f5705fbdfa99f77a
BasharAlZawahreh <zawahrehbashar@gmail.com>
2021-07-09 14:23:10 +0300

    Q1

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 console.log(arrowTitle)
 /* START CODE UNDER THIS LINE */
+
+let myFavoriteFood  = 'Maqlouba';
+// make sure that the variable is in the global scope
+const favoriteFood = function () {
+    myFavoriteFood = 'Mansaf';
+};
+  
+  favoriteFood(); // => the value of `myFavoriteFood` variable
+  
